12a36872124368d74c7c3c08a0580a3d294af595
Suhun Han <ssut@ssut.me>
2020-08-06 00:51:49 +0900

    feat(main): load .env, and register syncfusion license

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:twitch_speedtest/src/app.dart';
+import 'package:syncfusion_flutter_core/core.dart';
 
-void main() {
+void main() async {
+  await DotEnv().load('.env');
+  SyncfusionLicense.registerLicense(DotEnv().env['SYNCFUSION_LICENSE']);
+
   runApp(
       EasyLocalization(
         supportedLocales: [
