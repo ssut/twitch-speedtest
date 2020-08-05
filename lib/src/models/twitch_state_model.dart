9d32e7373fc24a63e0c96c4c59fc3822d6187199
Suhun Han <ssut@ssut.me>
2020-08-06 00:53:11 +0900

    feat(speedtest): it works

--- lib/src/models/twitch_state_model.dart
+++ lib/src/models/twitch_state_model.dart
@@ -1,13 +1,24 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/foundation.dart' as foundation;
 import 'package:flutter_hls_parser/flutter_hls_parser.dart';
+import 'package:pool/pool.dart';
+import 'package:twitch_speedtest/src/speedtest/jitter.dart';
+import 'package:twitch_speedtest/src/speedtest/latency.dart';
+import 'package:http/http.dart' as http;
 import 'package:twitch_speedtest/src/twitch/client.dart';
 import 'package:twitch_speedtest/src/twitch/servers.dart';
+import 'package:dio/dio.dart';
 
 enum TwitchLoadingState {
   Error,
+  NoServers,
   Standby,
   Preparing,
-  Testing,
+  TestingPing,
+  TestingSpeed,
+  Reporting,
   Done,
 }
 
@@ -15,8 +26,22 @@ class TwitchStateModel extends foundation.ChangeNotifier {
   TwitchClient client;
 
   List<List<Variant>> hlsVariantsInfos;
+  List<List<Variant>> variantsForTest = [[], []];
   Set<TwitchServer> twitchServers = Set();
   String servers = '';
+
+  List<int> _latencies = [];
+
+  int avgLatency;
+  int jitter;
+
+  double progress = 0.0;
+
+  Duration testDuration;
+  int bytesReceived;
+  int lastSpeedBytes;
+  int _lastCheckedMillis = 0;
+  int _lastCheckedBytes = 0;
 
   TwitchLoadingState state = TwitchLoadingState.Standby;
 
@@ -26,8 +51,16 @@ class TwitchStateModel extends foundation.ChangeNotifier {
     notifyListeners();
   }
 
+  int get lastLatency => _latencies.length > 0 ? _latencies.last : null;
+
   _clearTwitchServers() {
     twitchServers.clear();
+
+    notifyListeners();
+  }
+
+  _setProgress(double progress) {
+    this.progress = progress;
 
     notifyListeners();
   }
@@ -36,11 +69,128 @@ class TwitchStateModel extends foundation.ChangeNotifier {
     this.twitchServers = twitchServers;
   }
 
+  _setVariantsForTest(List<List<Variant>> list) {
+    variantsForTest = list ?? [[], []];
+  }
+
+  _calculateAvgLatency() {
+    final latencies = _latencies ?? [];
+    _setAvgLatency((latencies.reduce((a, b) => a + b) / latencies.length).toInt());
+  }
+
+  _setAvgLatency(int latency) {
+    avgLatency = latency;
+
+    notifyListeners();
+  }
+
+  _calculateJitter() {
+    final latencies = (_latencies ?? []).map((latency) => latency.toDouble()).toList();
+
+    _setJitter(Jitter.compute(latencies).toInt());
+  }
+
+  _setJitter(int jitter) {
+    this.jitter = jitter;
+
+    notifyListeners();
+  }
+
   _initClient() {
     this.client = TwitchClient();
   }
 
-  init({ int targetChannelCount = 15 }) async {
+  _updatePeriodically({ Duration interval, CancelToken token }) async {
+    for (;;) {
+      if (token.isCancelled) {
+        return;
+      }
+
+      if (_lastCheckedMillis < (testDuration.inMilliseconds / 100).floor() * 100) {
+        lastSpeedBytes = (bytesReceived ~/ testDuration.inMilliseconds) * 1000;
+        _lastCheckedMillis = testDuration.inMilliseconds;
+        _lastCheckedBytes = bytesReceived;
+      }
+
+      progress = min(1, testDuration.inMilliseconds / 10000);
+      notifyListeners();
+      await Future.delayed(interval);
+    }
+  }
+
+  _testSpeed({ List<List<String>> mediaUrls }) async {
+    final testUrls = [...mediaUrls[0], ...mediaUrls[1]];
+
+    CancelToken token = CancelToken();
+
+    bytesReceived = 0;
+    testDuration = Duration(milliseconds: 0);
+    lastSpeedBytes = 0;
+    _lastCheckedMillis = 0;
+    _lastCheckedBytes = 0;
+    notifyListeners();
+
+    final pool = new Pool(8, timeout: new Duration(seconds: 60));
+    _updatePeriodically(
+      token: token,
+      interval: Duration(milliseconds: 150),
+    );
+
+    int lastIndex = testUrls.length;
+    for (int i = 0; i < lastIndex && testDuration.inMilliseconds < 10000; i++) {
+      final url = testUrls[i];
+      if (url == null) {
+        break;
+      }
+
+      Response<ResponseBody> response;
+      try {
+        response = await Dio().get<ResponseBody>(url,
+          options: Options(
+            responseType: ResponseType.stream,
+          ),
+          cancelToken: token,
+        );
+      } catch (e) {
+        continue;
+      }
+
+      if (response == null) {
+        continue;
+      }
+
+      await pool.withResource(() async {
+        final completer = new Completer();
+        Stopwatch stopwatch = Stopwatch()..start();
+        // ignore: cancel_subscriptions
+        dynamic sub;
+        sub = response.data.stream.listen((value) {
+          if (token.isCancelled) {
+            sub?.cancel();
+            throw new Error();
+          }
+
+          bytesReceived += value.length;
+          testDuration += stopwatch.elapsed;
+          stopwatch.reset();
+        });
+
+        sub.onError(completer.complete);
+        sub.onDone(completer.complete);
+
+        await completer.future;
+//        sub.cancel();
+      });
+    }
+
+    _setProgress(1);
+    token.cancel();
+
+    lastSpeedBytes = (bytesReceived ~/ testDuration.inSeconds);
+    notifyListeners();
+  }
+
+  start({ int targetChannelCount = 20 }) async {
     if (state.index >= TwitchLoadingState.Preparing.index) {
       return;
     }
@@ -48,9 +198,11 @@ class TwitchStateModel extends foundation.ChangeNotifier {
     setLoadingState(TwitchLoadingState.Preparing);
     _initClient();
     _clearTwitchServers();
+    _setVariantsForTest(null);
+    _setProgress(0);
 
     try {
-      final streams = await client.getRecommendedStreams(count: 15);
+      final streams = await client.getRecommendedStreams(count: targetChannelCount);
       print(streams);
 
       hlsVariantsInfos = (await Future.wait(
@@ -66,8 +218,6 @@ class TwitchStateModel extends foundation.ChangeNotifier {
         }),
       )).where((element) => element != null).toList();
 
-      print(hlsVariantsInfos);
-
       Set<TwitchServer> servers = Set();
       for (var variants in hlsVariantsInfos) {
         final first = variants.first;
@@ -81,7 +231,90 @@ class TwitchStateModel extends foundation.ChangeNotifier {
         }
       }
 
+      if (servers.length == 0) {
+        setLoadingState(TwitchLoadingState.NoServers);
+        return;
+      }
+
       _setServers(servers);
+
+      List<List<Variant>> variantsForTest = [[], []];
+      // filter 2 of the highest bitrate hlsVariantInfos only
+      for (var hlsVariants in hlsVariantsInfos) {
+        var variants = List<Variant>.from(hlsVariants);
+        variants.sort((a, b) => b.format.bitrate - a.format.bitrate);
+        variants = variants.take(2).toList();
+
+        if (variants.length == 2) {
+          variantsForTest[0].add(variants[1]);
+          variantsForTest[1].add(variants[0]);
+        }
+      }
+
+      if (variantsForTest[0].length == 0 || variantsForTest[1].length == 0) {
+        setLoadingState(TwitchLoadingState.NoServers);
+        return;
+      }
+      _setVariantsForTest(variantsForTest);
+
+      // loading all variant urls
+      final mediaUrls = await Future.wait(
+        variantsForTest.map((variants) async {
+          var segmentUrls = await Future.wait(variants.map((variant) async {
+            final response = await http.get(variant.url);
+            var playlist = await HlsPlaylistParser.create().parseString(variant.url, response.body);
+
+            if (playlist is HlsMediaPlaylist) {
+              return playlist.segments.map((segment) => segment.url).toList();
+            }
+
+            return null;
+          }));
+
+          return segmentUrls.expand((element) => element).where((element) => element != null).toList();
+        })
+      );
+
+      // first comes: (1) 5 seconds (2)
+      final stopwatch = Stopwatch()..start();
+
+      setLoadingState(TwitchLoadingState.TestingPing);
+      final testList = [...mediaUrls[0], ...mediaUrls[1]];
+
+      var last = testList.length;
+      var i = 0;
+      for (; i < last; i++) {
+        final url = testList[i];
+        if (url == null) {
+          break;
+        }
+
+        final uri = Uri.parse(url);
+        try {
+          final latency = await Latency.test(uri);
+          if (latency != null) {
+            if (_latencies.length == 0) {
+              last = max(10000 ~/ latency, 200);
+            }
+
+            _latencies.add(latency);
+            _calculateAvgLatency();
+          }
+        } catch (e) {
+        } finally {
+          double progress = ++i / last;
+          _setProgress(progress);
+        }
+      }
+
+      _setProgress(1);
+      _calculateJitter();
+
+      setLoadingState(TwitchLoadingState.TestingSpeed);
+
+      await _testSpeed(
+        mediaUrls: mediaUrls,
+      );
     } catch (e) {
       print(e);
 
--- lib/src/views/speedtest_tab_view.dart
+++ lib/src/views/speedtest_tab_view.dart
@@ -1,9 +1,14 @@
+import 'dart:math';
+
+import 'package:cupertino_progress_bar/cupertino_progress_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_cupertino_settings/flutter_cupertino_settings.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:progress_state_button/iconed_button.dart';
 import 'package:progress_state_button/progress_button.dart';
 import 'package:provider/provider.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
 import 'package:twitch_speedtest/src/models/app_state_model.dart';
 import 'package:twitch_speedtest/src/models/twitch_state_model.dart';
 
@@ -12,6 +17,113 @@ class SpeedTestTabView extends StatelessWidget {
     return SpinKitFadingCircle(
       color: Colors.deepPurple,
       size: 50.0,
+    );
+  }
+
+  Widget _ping(TwitchStateModel state) {
+    final latency = state == TwitchLoadingState.TestingPing ? (state.lastLatency ?? state.avgLatency).toString() : (state.avgLatency).toString();
+
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(
+            width: 300,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text('Latency', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                    Text(latency, style: TextStyle(fontSize: 32)),
+                    Text('ms', style: TextStyle(fontSize: 16, color: Color(0xFFCACACA))),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text('Jitter', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                    Text(state.jitter.toString(), style: TextStyle(fontSize: 32)),
+                    Text('ms', style: TextStyle(fontSize: 16, color: Color(0xFFCACACA))),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _speed(TwitchStateModel state) {
+    final currentSpeed = ((state.lastSpeedBytes ?? 0.0) / 125000);
+    final value = min(currentSpeed, 150);
+
+    return SizedBox(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SfRadialGauge(
+                enableLoadingAnimation: true,
+                animationDuration: 3000,
+                axes: <RadialAxis>[
+                  RadialAxis(
+                    axisLineStyle: AxisLineStyle(thickness: 30),
+                    showTicks: false,
+                    minimum: 0,
+                    maximum: 150,
+                    annotations: <GaugeAnnotation>[
+                      GaugeAnnotation(
+                        widget: Container(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(currentSpeed.toStringAsFixed(2), style: TextStyle(fontSize: 38.0, fontWeight: FontWeight.bold)),
+                              Text('Mbps', style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.normal)),
+                            ],
+                          ),
+                        ),
+                        angle: 90,
+                        positionFactor: 0.5,
+                      ),
+                    ],
+                    pointers: <GaugePointer>[
+                      RangePointer(
+                        value: value,
+                        color: Colors.deepPurpleAccent.shade200,
+                        width: 30.0,
+                        enableAnimation: true,
+                      ),
+                      NeedlePointer(
+                        value: value,
+                        enableAnimation: true,
+                        needleStartWidth: 0,
+                        needleEndWidth: 3,
+                        needleColor: Colors.deepPurpleAccent.shade700,
+                        knobStyle: KnobStyle(
+                          color: Colors.white,
+                          borderColor: Colors.deepPurpleAccent.shade700,
+                          knobRadius: 0.06,
+                          borderWidth: 0.04,
+                        ),
+                        tailStyle: TailStyle(
+                          color: Colors.deepPurpleAccent.shade700,
+                          width: 3,
+                          length: 0.15,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 
@@ -82,17 +194,60 @@ class SpeedTestTabView extends StatelessWidget {
         if (appState.isLoading) {
           child = _appLoading;
         } else {
-          child = _standby(
-              state: twitchState.state,
-              onStartButtonPress: twitchState.init,
-          );
+          switch (twitchState.state) {
+            case TwitchLoadingState.NoServers:
+            case TwitchLoadingState.Error:
+            case TwitchLoadingState.Preparing:
+            case TwitchLoadingState.Standby:
+              child = _standby(
+                state: twitchState.state,
+                onStartButtonPress: twitchState.start,
+              );
+              break;
+
+            case TwitchLoadingState.TestingPing:
+//            default:
+              child = _ping(
+                twitchState,
+              );
+              break;
+
+            case TwitchLoadingState.TestingSpeed:
+              child = _speed(
+                twitchState,
+              );
+              break;
+        }
         }
 
         return CupertinoPageScaffold(
           navigationBar: CupertinoNavigationBar(
-            middle: Text('SpeedTest'),
+            padding: EdgeInsetsDirectional.zero,
+            middle: Stack(
+              overflow: Overflow.visible,
+              children: <Widget>[
+                Center(child: Text('SpeedTest')),
+                Positioned(
+                  left: -8,
+                  right: -8,
+                  bottom: 0,
+                  child: CupertinoProgressBar(
+                    value: twitchState.progress,
+                    trackColor: null,
+                  ),
+                ),
+              ],
+            ),
+//            middle: Text('SpeedTest'),
+
           ),
-          child: child,
+          child: SafeArea(
+            child: Column(
+              children: <Widget>[
+                Expanded(child: child),
+              ],
+            ),
+          ),
         );
       },
     );
